7345aa3bf1f946fbc5d5ce12a2e37b298bd4ea15
mapsut pasut <mpasut@riu.com>
2023-01-16 22:15:02 -0300

    creo el componente console

---
+++ src/components/Console.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Console() {
+  return (
+    <>
+      <div className="content">
+        [WIP CONSOLE]
+      </div>
+    </>
+  );
+}
+
+export default Console;
