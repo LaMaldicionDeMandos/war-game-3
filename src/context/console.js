e91252ac13e42799a08f10f4e9b74735985622c6
mapsut pasut <mpasut@riu.com>
2023-01-23 17:50:02 -0300

    fixeo los argumentos de los comandos

--- src/context/console.js
+++ src/context/console.js
@@ -5,16 +5,22 @@ import addCountryCommand from '../context/commands/addCountry.command';
 class Console {
   #processors = [addCountryCommand];
   execute(command, position) {
-    const expressions = this.#toArray(command);
-    const commandName = _.first(expressions);
-    const args = _.tail(expressions);
+    const [commandName, args] = this.#splitCommand(command);
     const processor = _.find(this.#processors, processor => processor.support(commandName));
     if (!processor) return Promise.reject(`Error: command ${commandName} not found`);
     return processor.execute(args, position);
   }
 
-  #toArray = (command) => {
-    return _.chain(command).split(' ').map((arg) => arg.trim()).value();
+  #splitCommand = (command) => {
+    const index = command.indexOf(' ');
+    const commandName = command.substring(0, index);
+    const args = command.substring(index + 1);
+
+    return [commandName, this.#parseParams(args)];
+  }
+
+  #parseParams(args) {
+    return _.chain(args).split('--').tail().map((arg) => arg.trim()).value();
   }
 }
 
