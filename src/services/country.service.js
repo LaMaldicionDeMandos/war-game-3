09002b94d0dcdf90e1e90371365aaa65225ecdf3
mapsut pasut <mpasut@riu.com>
2023-02-06 18:13:22 -0300

    implemento el post de city

--- src/services/country.service.js
+++ src/services/country.service.js
@@ -3,6 +3,17 @@ import axios from 'axios';
 const API_URL = process.env.REACT_APP_API_URL;
 
 class CountryService {
+  addCity(city) {
+    return axios
+      .post(
+        `${API_URL}/countries/${city.country}/city`,
+        city,
+        {}
+      )
+      .then(response => response.data)
+      .catch(e => Promise.reject(e.response.data));
+  }
+
   getCountry(id) {
     return axios
       .get(
